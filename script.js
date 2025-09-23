81612e11d19243c48cd151db1b64043216e04353
”gabriellaberko” <75434964+gabriellaberko@users.noreply.github.com>
2025-09-23 16:08:26 +0200

    capture the clicked button's text and create a function for printing a message based on it

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-/* 
- TO-DO: Filter
- - Put an event listener for clicks on the filter buttons
- - Toggle the "active" class when filter button is clicked
- - Use conditional statements to display a message to the user, based on the filter input
-*/
+// TO DO: Limit to only pick one sorting button at a time
+// Make sure you cannot chose "All" and other filter options at the same time
 
 const buttons = document.querySelectorAll(".btn");
 const filterButtons = document.querySelectorAll(".filter-container .btn");
 
-/* Add an event listener for every button in the node list and toggle the class active on click */
+buttonText = "";
+
+
+
+function printButtonText(buttonText) {
+  if (buttonText === "All") {
+    console.log("You chose all")
+  } else if (buttonText === "Asian") {
+    console.log("You chose Asian");
+  } else if (buttonText === "Italian") {
+    console.log("You chose Italian");
+  } else if (buttonText === "Mediterranean") {
+    console.log("You chose Mediterranean");
+  } else if (buttonText === "Middle Eastern") {
+    console.log("You chose Middle Eastern");
+  } else if (buttonText === "Mexican") {
+    console.log("You chose Mexican");
+  }
+}
+
+/* 
+- Add an event listener for every button in the node list and toggle the class active on click 
+- Get the clicked button's text and use it as an argument when calling the printButtonText function
+*/
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("active");
+    const buttonText = button.innerText;
+    printButtonText(buttonText);
   });
 }); 
+
