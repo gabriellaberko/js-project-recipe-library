a6f62e1a8af6d23fa6f52cb7450d2d3e3a5740b2
”gabriellaberko” <75434964+gabriellaberko@users.noreply.github.com>
2025-09-24 14:24:55 +0200

    separate function for sort and filter buttons and fix issue with buttons that should not be active at the same time

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 
 const buttons = document.querySelectorAll(".btn");
 const filterButtons = document.querySelectorAll(".filter-container .btn");
+const filterAllButton = document.getElementById("filter-all-button");
+const sortButtons = document.querySelectorAll(".sort-container .btn");
+
 
 
 buttonText = "";
@@ -34,16 +37,43 @@ const createMessage = buttonText => {
     addMessageToPlaceholder("The least popular recepies?");
   }};
 
+
 /* 
-- Add an event listener for every button in the node list and toggle the class active on click 
-- Get the clicked button's text and use it as an argument when calling the printButtonText function
+- Add an event listener for every button in the node list and add the class "active" only on the clicked button
+- Get the clicked button's text and use it as an argument when calling the createMessage function
 */
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.classList.toggle("active");
-    const buttonText = button.innerText;
-    // Only call the create message function if button is active
-    if (button.classList.contains("active")) {
+
+filterButtons.forEach((filterButton) => {
+  filterButton.addEventListener("click", () => {
+    const buttonText = filterButton.innerText;
+    // if the clicked button is "All", remove the class "active" from all the other buttons
+    if(buttonText === "All") {
+      filterButtons.forEach((filterButton) => filterButton.classList.remove("active"));
+      filterButton.classList.add("active");
+    // if the clicked button is any other, remove the class "active" from the filterAllButton and toggle the class on the clicked button
+    } else if(buttonText !== "All") {
+      const filterAllButton = document.getElementById("filter-all-button");
+      filterAllButton.classList.remove("active");
+      filterButton.classList.toggle("active");
+    }
+    // only call the create message function if button is active
+    if (filterButton.classList.contains("active")) {
+      createMessage(buttonText); 
+    }
+  });
+}); 
+
+
+sortButtons.forEach((sortButton) => {
+  sortButton.addEventListener("click", () => {
+    // remove the class 'active' from all sort buttons
+    sortButtons.forEach((sortButton) => sortButton.classList.remove("active"));
+    // add the class 'active' to the clicked sort button
+    sortButton.classList.add("active");
+    
+    const buttonText = sortButton.innerText;
+    // only call the create message function if button is active
+    if (sortButton.classList.contains("active")) {
       createMessage(buttonText); 
     }
   });
